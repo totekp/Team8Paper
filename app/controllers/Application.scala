8499017352480606ae80fcad41716504715ee32f
Kefu Zhou <kfer38@gmail.com>
2014-01-22 14:13:33 -0500

    example view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,8 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok("Team8Paper Project")
+    val list = (0 to 100).toList
+    Ok(views.html.index2(list))
   }
 
   def paperView(id: String) = play.mvc.Results.TODO
