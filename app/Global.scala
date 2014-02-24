73e2d284250f6fb24615ccd246a0dd4061ae803c
Kefu Zhou <kefu.zhou@tstllc.net>
2014-02-24 09:04:41 -0500

    ensure indices for next sprint

--- app/Global.scala
+++ app/Global.scala
@@ -1,9 +1,13 @@
 
+import models.Paper
 import play.filters.gzip.GzipFilter
 import play.api._
 import play.api.mvc._
-import scala.concurrent.Future
+import reactivemongo.api.indexes.{IndexType, Index}
+import scala.concurrent.{Await, Future}
 import play.api.libs.concurrent.Execution.Implicits._
+import services.PaperDAO
+import scala.concurrent.duration._
 
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
 
@@ -18,5 +22,14 @@ object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
       super.onHandlerNotFound(request)
     }
   }
+
+  override def onStart(app: Application) {
+    val f1 = PaperDAO.coll.indexesManager.ensure(Index(Seq(Paper.title -> IndexType.Descending)))
+    val f2 = PaperDAO.coll.indexesManager.ensure(Index(Seq(Paper.tags -> IndexType.Descending)))
+    val r = List(f1, f2)
+
+    assert(Await.result(Future.sequence(r), 30.seconds).forall(_ == true), "Indices cannot be ensured")
+  }
+
 
 }
