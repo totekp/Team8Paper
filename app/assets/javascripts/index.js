a57317a20a919f55db9a2778940985a2ec8f7760
Joon Ki Hong <Joon@r4-128-61-89-172.res.gatech.edu>
2014-03-04 12:46:17 -0500

    Added seconds to time.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -435,14 +435,17 @@ jQuery.extend({
 Date.prototype.formatDateTime = function(){
     var hours = this.getHours();
     var minutes = this.getMinutes();
+    var seconds = this.getSeconds();
     var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
     return this.getMonth() +
     "/" +  this.getDate() +
     "/" +  this.getFullYear() + " "
     + hours + ":"
-    + minutes + " "
+    + minutes + ":"
+    + seconds + " "
     + ampm;
 }
