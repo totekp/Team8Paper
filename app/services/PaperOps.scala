2c0b78bcaf484bc1e2dcb17a4c824dc855aae9ca
Kefu Zhou <kfer38@gmail.com>
2014-04-05 20:45:17 -0400

    keep diffs simple

--- app/services/PaperOps.scala
+++ app/services/PaperOps.scala
@@ -6,31 +6,31 @@ import scala.concurrent.duration._
 
 object PaperOps {
 
-  def createDiff(from: Paper, to: Paper, origin: Vector[String]): PaperDiff = {
-    val modified = to.modified
-    val fromJson = Paper.model2json(from)
-    val toJson = Paper.model2json(to)
-
-    val added = JsonUtil.addedFields(fromJson, toJson).toMap
-    val deleted = JsonUtil.deletedKeys(fromJson, toJson)
-    val jsonDiff = JsonDiff(deleted, added)
-    PaperDiff(modified, jsonDiff, origin)
-  }
-
-  def reduceByInterval(p: Paper, interval: Duration = 5.minutes): Paper = {
-    val diffs = p.diffs.sortWith(_.modified > _.modified)
-    // Dumb check
-    assert(diffs.map(_.modified).size == diffs.map(_.modified).distinct.size, "Diffs must have different modified values")
-    val reducedDiffs = diffs.foldLeft(Vector.empty[PaperDiff]) {
-      (acc, d) =>
-        if (acc.isEmpty || math.abs(acc.last.modified - d.modified) > interval.toMillis) {
-          d +: acc
-        } else {
-          val updatedDiff = PaperDiff.mergeOldToNew(acc.head, d)
-          acc.updated(0, updatedDiff)
-        }
-    }
-    p.copy(diffs = reducedDiffs)
-  }
+//  def createDiff(from: Paper, to: Paper, origin: Vector[String]): PaperDiff = {
+//    val modified = to.modified
+//    val fromJson = Paper.model2json(from)
+//    val toJson = Paper.model2json(to)
+//
+//    val added = JsonUtil.addedFields(fromJson, toJson).toMap
+//    val deleted = JsonUtil.deletedKeys(fromJson, toJson)
+//    val jsonDiff = JsonDiff(deleted, added)
+//    PaperDiff(modified, jsonDiff, origin)
+//  }
+//
+//  def reduceByInterval(p: Paper, interval: Duration = 5.minutes): Paper = {
+//    val diffs = p.diffs.sortWith(_.modified > _.modified)
+//    // Dumb check
+//    assert(diffs.map(_.modified).size == diffs.map(_.modified).distinct.size, "Diffs must have different modified values")
+//    val reducedDiffs = diffs.foldLeft(Vector.empty[PaperDiff]) {
+//      (acc, d) =>
+//        if (acc.isEmpty || math.abs(acc.last.modified - d.modified) > interval.toMillis) {
+//          d +: acc
+//        } else {
+//          val updatedDiff = PaperDiff.mergeOldToNew(acc.head, d)
+//          acc.updated(0, updatedDiff)
+//        }
+//    }
+//    p.copy(diffs = reducedDiffs)
+//  }
 
 }
